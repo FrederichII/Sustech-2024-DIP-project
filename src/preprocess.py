8ec814be4f4a52ddbd4f022e5ef2770e6edad518
Zhang <12012724@mail.sustech.edu.cn>
2024-04-21 21:59:20 +0800

    rectification okay

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,5 @@
 import os
 import cv2
-import freetype
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
@@ -49,8 +48,22 @@ def ImgShow(imgpath, box, points, label):
     DrawPoint(im, points)
     im = DrawLabel(im, label)
     cv2.imshow('img', im)
+def get_dst_points(box):
+    top_left = box[0]
+    bottom_right = box[1]
+    top_right = [bottom_right[0], top_left[1]]
+    bottom_left=  [top_left[0],bottom_right[1]]
+    dst = np.array([top_left,top_right,bottom_left,bottom_right],dtype=np.float32)
+    return dst
 
-
+def rectification(imgpath, src_points, dst_points):
+    src_points = np.array([src_points[0],src_points[1],src_points[3],src_points[2]], dtype=np.float32)
+    print(src_points,"\n")
+    print(dst_points,"\n")
+    img = cv2.imread(imgpath)
+    M = cv2.getPerspectiveTransform(src_points, dst_points)
+    corrected_img = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))
+    return corrected_img
 
 def get_license_info(imgpath):
     # 图像路径（根据数据库的实际存放路径更改）
@@ -97,18 +110,37 @@ def get_ROI(imgpath, box):
     return cropped_image
 
 if __name__ == '__main__':
-    imgpath = 'D:/CCPD2019/ccpd_base/01-90_87-240&501_441&563-437&567_237&561_230&490_430&496-0_0_2_12_32_26_26-180-20.jpg'
+    name = '0237607758621-96_76-316&414_572&502-588&511_313&479_299&396_574&428-0_0_3_27_32_27_33-121-51.jpg'
+    imgpath = 'D:/CCPD2019/ccpd_base/'+name
     information = get_license_info(imgpath)
     [imgname, box, points, label] = [information.get('imgname'), information.get('box'), information.get('points'), information.get('label')]
     print(points)
     print(label)
     print(box)
     print(imgname)
-    ROI = get_ROI(imgpath,box)
 
+    ROI = get_ROI(imgpath,box)
+    img_rect = rectification(imgpath,points,get_dst_points(box))
     # 显示
     ImgShow(imgpath, box, points, label)
-    cv2.imshow('ROI',ROI)
+    tmp_filename = "../preprocess/" + imgname + ".jpg"
+    print(tmp_filename)
+    cv2.imwrite(tmp_filename, img_rect)
+    ROI_corrected = get_ROI(tmp_filename,box)
+    ROI_gray = cv2.resize(ROI_corrected,(440,140))
+    ROI_gray=  cv2.resize(ROI_gray,dsize=None, fx=3,fy=3,interpolation=cv2.INTER_CUBIC)
+    ROI_gray = cv2.cvtColor(ROI_gray, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('ROI', ROI_gray)
+    #ROI_gray = cv2.Sobel(ROI_gray, cv2.CV_64F, 1, 0, ksize=5)
+    _,ROI_gray = cv2.threshold(ROI_gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    ROI_gray = cv2.normalize(ROI_gray, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    #ROI_gray = cv2.equalizeHist(ROI_gray)
+    #ROI_gray = cv2.resize(ROI_gray, None, fx=4, fy=4, interpolation=None)
+
+    #ROI_gray = cv2.Laplacian(ROI_gray, cv2.CV_64F, dst=None, ksize=5)
+    #ROI_gray = cv2.normalize(ROI_gray, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    cv2.imshow('ROI_threshold',ROI_gray)
+    cv2.imshow('img_rect',img_rect)
     cv2.waitKey()
 
 
