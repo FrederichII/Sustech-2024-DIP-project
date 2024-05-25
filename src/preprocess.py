4823b41c3b12ed1b851a338a2ea911229592f069
zhang <13802502592@163.com>
2024-05-26 03:15:18 +0800

    .

--- src/preprocess.py
+++ src/preprocess.py
@@ -37,7 +37,7 @@ def DrawPoint(im, points):
 def DrawLabel(im, label):
     im = Image.fromarray(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
     draw = ImageDraw.Draw(im)
-    draw.text((30,30), label, font=ImageFont.truetype('msyh.ttc', 40), fill=(255,0,0))
+    draw.text((30,30), label, font=ImageFont.truetype('yahei_mono_0.ttf', 40), fill=(255,0,0))
     im = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
     return im
 
@@ -48,6 +48,8 @@ def ImgShow(imgpath, box, points, label):
     DrawPoint(im, points)
     im = DrawLabel(im, label)
     cv2.imshow('img', im)
+    
+    
 def get_dst_points(box):
     top_left = box[0]
     bottom_right = box[1]
@@ -56,6 +58,7 @@ def get_dst_points(box):
     dst = np.array([top_left,top_right,bottom_left,bottom_right],dtype=np.float32)
     return dst
 
+# 投影变换
 def rectification(imgpath, src_points, dst_points):
     src_points = np.array([src_points[0],src_points[1],src_points[3],src_points[2]], dtype=np.float32)
     print(src_points,"\n")
@@ -101,17 +104,39 @@ def get_license_info(imgpath):
 
 def get_ROI(imgpath, box):
     img = cv2.imread(imgpath)
+    print(img)
     top_left, bottom_right = box[0], box[1]
     left, top = min(top_left[0], bottom_right[0]), min(top_left[1], bottom_right[1])
     right, bottom = max(top_left[0], bottom_right[0]), max(top_left[1], bottom_right[1])
+    print(img is None)
     # 提取矩形区域
     cropped_image = img[top:bottom, left:right]
 
     return cropped_image
 
+def seperate(ROI):
+    seg1 = ROI[:,0:180]
+    seg2 = ROI[:,180:360]
+    seg3 = ROI[:,400:580]    
+    seg4 = ROI[:,580:750]
+    seg5 = ROI[:,750:920]
+    seg6 = ROI[:,920:1090]
+    seg7 = ROI[:,1090:1320]
+    return seg1,seg2,seg3,seg4,seg5,seg6,seg7
+
+def preprocess(img_directory):
+    for item in os.listdir(img_directory): # 遍历访问待处理图片路径下的每张图片
+        item_path = os.path.join(img_directory,item)
+        result_dir = '/home/zhang/DIP/Sustech-2024-DIP-project/result'
+        folder_path = os.path.join(result_dir,item)
+        os.mkdir(folder_path) # 单张图片的预处理结果都放在这个文件夹里（包含7个.jpg文件，7个字符）
+        
+        
+
+
 if __name__ == '__main__':
-    name = '0237607758621-96_76-316&414_572&502-588&511_313&479_299&396_574&428-0_0_3_27_32_27_33-121-51.jpg'
-    imgpath = 'D:/CCPD2019/ccpd_base/'+name
+    name = '01-86_91-298&341_449&414-458&394_308&410_304&357_454&341-0_0_14_28_24_26_29-124-24.jpg'
+    imgpath = '/mnt/d/CCPD2019/ccpd_base/'+name
     information = get_license_info(imgpath)
     [imgname, box, points, label] = [information.get('imgname'), information.get('box'), information.get('points'), information.get('label')]
     print(points)
@@ -123,7 +148,7 @@ if __name__ == '__main__':
     img_rect = rectification(imgpath,points,get_dst_points(box))
     # 显示
     ImgShow(imgpath, box, points, label)
-    tmp_filename = "../preprocess/" + imgname + ".jpg"
+    tmp_filename = "/home/zhang/DIP/Sustech-2024-DIP-project/preprocess/" + imgname + ".jpg"
     print(tmp_filename)
     cv2.imwrite(tmp_filename, img_rect)
     ROI_corrected = get_ROI(tmp_filename,box)
@@ -134,6 +159,7 @@ if __name__ == '__main__':
     #ROI_gray = cv2.Sobel(ROI_gray, cv2.CV_64F, 1, 0, ksize=5)
     _,ROI_gray = cv2.threshold(ROI_gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     ROI_gray = cv2.normalize(ROI_gray, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    word1,word2,word3,word4,word5,word6,word7 = seperate(ROI_gray)
     #ROI_gray = cv2.equalizeHist(ROI_gray)
     #ROI_gray = cv2.resize(ROI_gray, None, fx=4, fy=4, interpolation=None)
 
@@ -141,6 +167,13 @@ if __name__ == '__main__':
     #ROI_gray = cv2.normalize(ROI_gray, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
     cv2.imshow('ROI_threshold',ROI_gray)
     cv2.imshow('img_rect',img_rect)
+    cv2.imshow('seg1',word1)
+    cv2.imshow('seg2',word2)
+    cv2.imshow('seg3',word3)
+    cv2.imshow('seg4',word4)
+    cv2.imshow('seg5',word5)
+    cv2.imshow('seg6',word6)
+    cv2.imshow('seg7',word7)
     cv2.waitKey()
 
 
